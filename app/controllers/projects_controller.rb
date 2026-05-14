925b93d962f092c0e726972da290dc9a290dc489
Igor Marçal <igormdj18@gmail.com>
2026-05-14 14:58:30 -0300

    Adicionei  ao controllerr e corrigi o erro de método indefinido.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,28 @@
 class ProjectsController < ApplicationController
-  def index
+  def index 
     @projects = Project.all
   end
 
-  def show
-    @project = Project.find(params[:id])
+  def show 
+    @project = Project.find(params[:id]) 
+  end
+
+  def new 
+    @project = Project.new
+  end
+
+  def create 
+    @project = Project.new(project_params)
+
+    if @project.save 
+      redirect_to projects_path 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def project_params
+    params.expect(project: [ :title, :description])
   end
 end
