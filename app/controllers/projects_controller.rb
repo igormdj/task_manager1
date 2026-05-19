b6e7bfc0e61dd81489cd9b0dca8687d18e61198b
Igor Marçal <igormdj18@gmail.com>
2026-05-19 11:40:45 -0300

    incluídos edit, update e destroy, correção de turbo method, finalizando exclusão de projetos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,25 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+        redirect_to projects_path, notice: "Projeto atualizado com sucesso!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Projeto deletado com sucesso!"
+  end
+
   private
   def project_params
     params.expect(project: [ :title, :description])
