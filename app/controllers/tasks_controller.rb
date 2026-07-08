53b6cc6e85d1a9273100ddbff25c1ecada7551c2
Igor Marçal <igormdj18@gmail.com>
2026-07-08 14:32:13 -0300

    Feature: Adicionado campo de notas nas tarefas via migration
    Atualizado o banco de dados com a coluna notes, liberado nos strong parameters do controller e estruturado na view.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :description, :completed, :due_date, :priority, :document)
+    params.require(:task).permit(:title, :description, :completed, :due_date, :priority, :document, :notes)
   end
 end
